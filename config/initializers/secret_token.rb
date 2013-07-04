64602aa8629675603e01d568e34208aff1270e20
shaffer-wv <shaffer-wv@outlook.com>
2013-07-04 09:52:55 -0400

    Created article controller, model, index view and show view

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBlog::Application.config.secret_token = '98df7244ecd34b148f78463e2b3858896c1c96cba7604839bc8cdfb30f8e43ac3b3dcb51e242c3c612c7a5af880107b561b156d4525a7892d390fe27d61eb337'
