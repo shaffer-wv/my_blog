9b79c422a38d18b4e4eca759382d1b1401d698fa
shaffer-wv <shaffer-wv@outlook.com>
2013-07-04 12:16:47 -0400

    Added form based workflow

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,34 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(params[:article])
+		if @article.save
+			redirect_to article_path(@article)
+		else
+			redirect_to action: new
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id]).destroy
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update_attributes(params[:article])
+			redirect_to article_path(@article)
+		else
+			redirect_to action: edit
+		end
+	end
+
 end
