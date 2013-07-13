13b33668a0fcc863f23dab2f1e454739657eec3c
shaffer-wv <shaffer-wv@outlook.com>
2013-07-13 14:42:18 -0400

    Added recent posts links to sidebar.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,4 +3,8 @@ module ArticlesHelper
 	def latest_article
       @article = Article.order("created_at").last()
     end
+
+    def recent_articles
+    	@articles = Article.order("created_at desc").limit(3)
+    end
 end
